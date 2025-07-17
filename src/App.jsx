3311efd25789ea0bbd3a6807e4a9a8c20942f5ee
Bharati <devanibharati1998@gmail.com>
2025-07-17 21:11:07 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Viewdata from "./Viewdata";
+
+
+function App() {
+
+  return (
+    <>
+   <h1>Hello</h1>
+  <Viewdata/> 
+  
+    </>
+  )
+}
+
+export default App
---
+++ src/Viewdata.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+
+const Viewdata=()=>{
+
+    const [users, setusers] = useState([])
+    useEffect(()=>{
+        axios.get("https://geton.yarainfotech.com/get-data.php").then((Response)=>{
+            setusers(Response.data)
+        })
+    })
+    return(
+        <>
+           <table style={{border:"1px solid black", textAlign:"center"}}>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th></th>
+                <th></th>
+            </tr>
+            {
+                users.map((e)=>{
+                    return(
+                        <>
+                        <tr>
+                            <td>{e.id}</td>
+                            <td>{e.name}</td>
+                            <td>{e.email}</td>
+                            <td>{e.password}</td>
+                            <td><button>Edit</button></td>
+                            <td><button>Delete</button></td>
+                        </tr>
+                        </>
+                    )
+                })
+            }
+           </table>
+        </>
+    )
+}
+export default Viewdata;
